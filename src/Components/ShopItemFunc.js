a73f21bcd3bb9d7f41c2dbe9cac7bf81ae553aef
Alexandr Korobov <100015046+dktwho@users.noreply.github.com>
2022-09-29 16:38:35 +0300

    Update ShopItemFunc.js

--- src/Components/ShopItemFunc.js
+++ src/Components/ShopItemFunc.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import {item} from '../App';
 import '../App.css'
 
- export  function ShopItemFunc  ()  {
+ export  function ShopItemFunc  ({item})  {
  
-
   return (
-    <div class="main-content">
+    <div className="main-content">
     <h2>{item.brand}</h2>
     <h1>{item.title}</h1>
     <h3>{item.description}</h3>
-    <div class="description">
+    <div className="description">
       {item.descriptionFull}
     </div>
-    <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-    <div class="divider"></div>
-    <div class="purchase-info">
-      <div class="price">{item.currency}{item.price}{'.00'}</div>
+    <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+    <div className="divider"></div>
+    <div className="purchase-info">
+      <div className="price">{item.currency}{item.price}{'.00'}</div>
       <button>Добавить в корзину</button>
     </div>
   </div>
